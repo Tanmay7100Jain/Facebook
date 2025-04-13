38ef1b81e1622b275c4281d9eab6bfdb5cc2ecbc
Tanmay7100Jain <tanmayjain.24.dce@iite.indusuni.ac.in>
2025-04-13 16:56:14 +0530

    Facebook

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <div className="p-4">
+      <h1 className="text-4xl font-bold mb-4">Menu</h1>
+
+      <div className="flex items-center gap-2 w-72 border rounded-full px-2 shadow-sm mb-6">
+        <img src="Seacrh1.svg" className="h-6 w-6" alt="Search Icon" />
+        <input
+          type="text"
+          placeholder="Search Menu"
+          className="outline-none w-full placeholder:text-lg"
+          aria-label="Search Menu"
+        />
+      </div>
+
+      <div className="text-xl font-semibold mb-4">Social</div>
+
+      <div className="flex items-center mb-4 gap-3">
+        <img src="download.png" className="h-8 w-8" alt="User Icon" />
+        <span className="text-2xl font-black">Welcome</span>
+      </div>
+      <div className="flex items-center mb-4 gap-3">
+        <img src="img11.png" className="h-8 w-8" alt="User Icon" />
+        <div className="text-2xl font-black">Events</div>
+        <span className="">Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, illum.</span>
+      </div>
+      <div className="flex items-center mb-4 gap-3">
+        <img src="download.png" className="h-8 w-8" alt="User Icon" />
+        <span className="text-2xl font-black">Welcome</span>
+      </div>
+      <div className="flex items-center mb-4 gap-3">
+        <img src="download.png" className="h-8 w-8" alt="User Icon" />
+        <span className="text-2xl font-black">Welcome</span>
+      </div>
+      <div className="flex items-center mb-4 gap-3">
+        <img src="download.png" className="h-8 w-8" alt="User Icon" />
+        <span className="text-2xl font-black">Welcome</span>
+      </div>
+    
+    </div>
+  );
+};
 
 export default Menu;
